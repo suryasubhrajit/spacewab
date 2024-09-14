0d6c3994e93a2ab6d903c880045fe4b189167157
Surya <suryasubhrajit@gmail.com>
2024-09-15 00:02:07 +0530

    deleting navigation folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Booking from './pages/Booking';
+import Explore from './pages/Explore';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Home />}>
+      <Route index element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='booking' element={<Booking />} />
+      <Route path='explore' element={<Explore />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
