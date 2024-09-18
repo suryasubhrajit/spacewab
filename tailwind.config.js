6bd4c5a08d4487b840e24434a1534ba5d2feab79
Surya <suryasubhrajit@gmail.com>
2024-09-18 19:53:47 +0530

    changes of tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
-const flowbite = require("flowbite-react/tailwind");
-
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    "./src/**/*.{js,jsx,ts,tsx,css}",
+    "./src/**/**/*.{js,jsx,ts,tsx.css}",
+    "./src/*.{css,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    // ...
-    flowbite.plugin(),
-  ],
-};
+  plugins: [],
+}
