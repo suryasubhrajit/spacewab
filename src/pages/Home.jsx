6edea7848065045a2de567176a778589eb24aff6
Surya <suryasubhrajit@gmail.com>
2024-09-18 19:47:40 +0530

    setting up tailwind css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,11 @@ import Header from '../components/_header'
 const Home = () => {
     return (
         <div>
-            Home
             <Header />
+            <h1>Home</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     )
 }
