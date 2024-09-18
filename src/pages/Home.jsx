470613188dfa5e1be569018013486a8b53acbccc
Santosh Padhiali <santoshpadhiali007@gmail.com>
2024-09-18 22:05:38 +0530

    clean all the code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,7 @@ const Home = () => {
     return (
         <div>
             <Header />
-            <h1>Home</h1>
-            <h1 className="text-3xl font-bold underline">
-                santosh Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae natus eum voluptatem?
-                ghzgjagjdah
-            </h1>
+            
         </div>
     )
 }
