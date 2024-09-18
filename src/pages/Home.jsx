f6520d2fe4aed4dfa85faa28f3d2477a3d2f4729
Santosh padhaiali <santoshpadhaiali007@gmail.com>
2024-09-18 19:57:32 +0530

    some changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Header from '../components/_header'
 
+
 const Home = () => {
     return (
-        <div>
-            Home
-            <Header />
-        </div>
+    <>
+    
+    </>
     )
 }
 
